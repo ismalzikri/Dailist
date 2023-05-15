5afd657f30ecd9f64f4afa383061b07cac1d66b6
Laravel007 <zikriismal42@gmail.com>
2023-05-15 14:36:00 +0700

    feat(App.tsx): add feature edit
    additional edit features

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { nanoid } from "nanoid";
 import { Button, EmptyPage, Modal } from "./components";
 import checklistIcon from "./assets/checklist.svg";
+import checkEditIcon from "./assets/check-mark-box-icon.svg";
 import "./App.css";
 
 type TodoItem = {
@@ -11,10 +12,12 @@ type TodoItem = {
 };
 
 function App() {
-  const [isModalOpen, setIsModalOpen] = useState(false);
-
-  // TODO: use nanoid/cuid to generate random id
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+  const [isEdit, setIsEdit] = useState<boolean>(false);
   const [todoItems, setTodoItems] = useState<TodoItem[]>([]);
+  const [selectedItemIndex, setSelectedItemIndex] = useState<number | null>(
+    null
+  );
 
   const handleToggleModal = () => {
     setIsModalOpen(!isModalOpen);
@@ -42,6 +45,33 @@ function App() {
     setTodoItems(newTodoItems);
   };
 
+  const handleEditTodoItem = (newValue: string) => {
+    if (selectedItemIndex === null) return;
+
+    const updatedTodoItems = [...todoItems];
+    updatedTodoItems[selectedItemIndex].title = newValue;
+    setTodoItems(updatedTodoItems);
+    setIsEdit(false);
+    setSelectedItemIndex(null);
+  };
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (selectedItemIndex === null) return;
+
+    const updatedTodoItems = [...todoItems];
+    updatedTodoItems[selectedItemIndex].title = event.target.value;
+    setTodoItems(updatedTodoItems);
+  };
+
+  const handleInputBlur = () => {
+    setIsEdit(false);
+    setSelectedItemIndex(null);
+  };
+
+  const handleDeleteAllCompleted = () => {
+    setTodoItems([]);
+  };
+
   return (
     <main className="App">
       <div className="wrapper">
@@ -57,16 +87,42 @@ function App() {
                   .filter((todoItem) => !todoItem.finished)
                   .map((todoItem, index) => (
                     <li key={todoItem.id}>
-                      <span>
-                        0{index + 1}. {todoItem.title}
-                      </span>
-                      <img
-                        width={26}
-                        height={26}
-                        src={checklistIcon}
-                        alt="checklist-icon"
-                        onClick={() => handleMarkAsCompleted(todoItem.id)}
-                      />
+                      {selectedItemIndex === index ? (
+                        isEdit ? (
+                          <input
+                            className="wrapper__upcoming__items--input"
+                            type="text"
+                            value={todoItem.title}
+                            onChange={handleInputChange}
+                            onBlur={handleInputBlur}
+                          />
+                        ) : (
+                          <span onClick={() => setIsEdit(!isEdit)}>
+                            0{index + 1}. {todoItem.title}
+                          </span>
+                        )
+                      ) : (
+                        <span onClick={() => setSelectedItemIndex(index)}>
+                          0{index + 1}. {todoItem.title}
+                        </span>
+                      )}
+                      {selectedItemIndex === index && isEdit ? (
+                        <img
+                          width={24}
+                          height={24}
+                          src={checkEditIcon}
+                          alt="checklist-edit-icon"
+                          onClick={() => handleEditTodoItem(todoItem.title)}
+                        />
+                      ) : (
+                        <img
+                          width={26}
+                          height={26}
+                          src={checklistIcon}
+                          alt="checklist-icon"
+                          onClick={() => handleMarkAsCompleted(todoItem.id)}
+                        />
+                      )}
                     </li>
                   ))}
               </ul>
@@ -75,7 +131,12 @@ function App() {
             <div className="wrapper__finished">
               <div className="wrapper__finished__top">
                 <span className="wrapper__finished__top__title">Finished</span>
-                <button className="wrapper__finished__top__clear">Clear</button>
+                <button
+                  onClick={handleDeleteAllCompleted}
+                  className="wrapper__finished__top__clear"
+                >
+                  Clear
+                </button>
               </div>
 
               <ul className="wrapper__finished__items">
